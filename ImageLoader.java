6590de5a2c8bdebb8f822d9895f3e20818740703
Ubspy <jack-m@live.com>
2016-10-01 10:23:36 -0500

    Added ImageLoader
    Now you can run this as an executable jar file

---
+++ ImageLoader.java
@@ -0,0 +1,21 @@
+import java.io.InputStream;
+
+final public class ImageLoader
+{
+	public static InputStream load(String path)
+	{
+		InputStream input = ImageLoader.class.getResourceAsStream(path);
+		
+		if(input == null)
+		{
+			input = ImageLoader.class.getResourceAsStream("\\" + path);
+		}
+		
+		if(input == null)
+		{
+			input = ImageLoader.class.getResourceAsStream("/" + path);
+		}
+		
+		return input;
+	}
+}
